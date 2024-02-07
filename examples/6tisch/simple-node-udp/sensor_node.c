e8c754abe19d4f98728828bac044b8ad1b85b303
YanboChenA <xt20786@bristol.ac.uk>
2024-02-07 15:28:16 +0000

    Rewrite the structure of sensor node, create a new function to send sensor data

--- examples/6tisch/simple-node-udp/sensor_node.c
+++ examples/6tisch/simple-node-udp/sensor_node.c
@@ -1,6 +1,7 @@
 #include "contiki.h"
 #include "net/ipv6/simple-udp.h"
 #include "net/mac/tsch/tsch.h"
+#include "net/mac/tsch/tsch-schedule.h"
 #include "lib/random.h"
 #include "sys/node-id.h"
 #include <string.h>
@@ -10,7 +11,7 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 #define UDP_PORT	8765
-#define SEND_INTERVAL		  (30 * CLOCK_SECOND)
+#define SEND_INTERVAL		  (60 * CLOCK_SECOND)
 
 #include "sensor_utils.h" // Include the sensor_utils.h file form ../../utils folder
 #include "sensor_data.h" // Include the sensor data file from ../../data/sensor_data folder
@@ -48,75 +49,68 @@ rx_callback( struct simple_udp_connection *c,
     }
 }
 
-PROCESS_THREAD(node_process, ev, data)
-{
+
+static void send_sensor_data(uip_ipaddr_t *dest_ipaddr, struct etimer *timer) {
+    static int sensor_index = 0; // Initialize the sensor index
+    static SensorData current_data; // Static to keep track of the current sensor data across function calls
+    SensorData next_data; // Initialize the next data struct
+
+    static uint32_t seqnum = 0;
+    char sensor_id = 'A' + (node_id - 1); // Set node id
+
+    // Logic to get current and next sensor data
+    if (sensor_index == 0 || current_data.timestamp == 0) {
+        current_data = get_sensor_data(sensor_id, sensor_index);
+    }
+    next_data = get_sensor_data(sensor_id, sensor_index + 1);
+
+    if (next_data.timestamp != 0) {
+        // Calculate the time interval between two readings and adjust the timer
+        clock_time_t next_interval = (next_data.timestamp - current_data.timestamp) * CLOCK_SECOND;
+        etimer_set(timer, next_interval);
+        current_data = next_data;
+        sensor_index++;
+    } else {
+        // Reset the sensor index and current data if no next data
+        sensor_index = 0;
+        memset(&current_data, 0, sizeof(current_data));
+        etimer_set(timer, SEND_INTERVAL);
+    }
+
+    // Send the sensor data
+    seqnum++;
+    LOG_INFO("Sending to ");
+    LOG_INFO_6ADDR(dest_ipaddr);
+    LOG_INFO_(", seqnum %" PRIu32 ", Sensor ID: %c\n", seqnum, sensor_id);
+    
+    simple_udp_sendto(&udp_conn, &current_data, sizeof(current_data), dest_ipaddr);
+}
+
+PROCESS_THREAD(node_process, ev, data) {
     static struct etimer periodic_timer;
     static uip_ipaddr_t dest_ipaddr;
-    static uint32_t seqnum;
-
-    static int sensor_index = 0; // Initialize the sensor index
-    SensorData current_data; // Initialize the current data struct
-    SensorData next_data; // Initialize the next data struct
-    memset(&current_data, 0, sizeof(current_data));
-    memset(&next_data, 0, sizeof(next_data));
-
 
     PROCESS_BEGIN();
 
-    /* Initialize UDP Connection for root node or sub node */
-    simple_udp_register(&udp_conn, UDP_PORT, NULL,
-                UDP_PORT, rx_callback);
-
-    etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
+    simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, rx_callback);
+    
     if (node_id == 1) { 
-      NETSTACK_ROUTING.root_start();
+        NETSTACK_ROUTING.root_start();
     }
     NETSTACK_MAC.on();
 
-    /* Set node id and son node id */
-    char sensor_id = 'A' + (node_id - 1);
+    etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
 
     while(1) {
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
-        if(NETSTACK_ROUTING.node_is_reachable() &&
-            NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
-                
-            // Get the current data for the given sensor ID and its occurrence index
-            // If current_data is not exist, get the first data of the sensor
-            if (sensor_index == 0 || current_data.timestamp == 0) {
-                current_data = get_sensor_data(sensor_id, sensor_index);
-            }
-
-            // Get the next data for the given sensor ID and its occurrence index
-            next_data = get_sensor_data(sensor_id, sensor_index + 1);
-
-            if (next_data.timestamp != 0) {
-                // Get the time interval between two consecutive readings of the same sensor
-                unsigned long next_interval = next_data.timestamp - current_data.timestamp;
-                etimer_set(&periodic_timer, next_interval);
-                current_data = next_data;
-                sensor_index++;
-            } else {
-                // If the next data is not exist, reset the sensor index and current data
-                etimer_set(&periodic_timer, SEND_INTERVAL);
-                sensor_index = 0;
-                memset(&current_data, 0, sizeof(current_data));
-            }
-
-            /*For the root node Send message to the network root node */
-            seqnum++;
-            LOG_INFO("Send to ");
-            LOG_INFO_6ADDR(&dest_ipaddr);
-            LOG_INFO_(", seqnum %" PRIu32 "\n", seqnum);
-            
-            simple_udp_sendto(&udp_conn, &current_data, sizeof(current_data), &dest_ipaddr);
-
-            // LOG_INFO_("Sensor ID: %c, Data Size: %d\n", sensor_id, sizeof(current_data));
-
+        if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+            send_sensor_data(&dest_ipaddr, &periodic_timer);
+        } else {
+            // Reset the timer if the network is not reachable or the root IP address cannot be obtained
+            etimer_set(&periodic_timer, SEND_INTERVAL);
         }
-
-    etimer_set(&periodic_timer, SEND_INTERVAL);
     }
+
     PROCESS_END();
 }

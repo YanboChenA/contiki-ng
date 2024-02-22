6a9212a446cb622254ffd82af67bc10fc8d090c6
YanboChenA <xt20786@bristol.ac.uk>
2024-02-22 09:36:59 +0000

    2/22

--- IRP/Node_with_config/sensor_node.c
+++ IRP/Node_with_config/sensor_node.c
@@ -6,6 +6,7 @@
 #include "sys/node-id.h"
 #include <string.h>
 #include <stdlib.h>
+#include "net/packetbuf.h"
 
 // #include "net/routing/rpl-lite/rpl.h"
 // #include "net/rpl/rpl-private.h"
@@ -24,6 +25,8 @@
 
 static struct simple_udp_connection udp_conn;
 
+
+
 static void 
 rx_callback( struct simple_udp_connection *c,
                   const uip_ipaddr_t *sender_addr,
@@ -31,26 +34,36 @@ rx_callback( struct simple_udp_connection *c,
                   const uip_ipaddr_t *receiver_addr,
                   uint16_t receiver_port,
                   const uint8_t *data,
-                  uint16_t datalen)
-{
+                  uint16_t datalen){   
+    int16_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+    uint8_t lqi = packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY);
+
     static uint32_t seqnum;
     if(datalen >= sizeof(seqnum)) {
         memcpy(&seqnum, data, sizeof(seqnum));
 
         LOG_INFO("Received from ");
         LOG_INFO_6ADDR(sender_addr);
-        LOG_INFO_(", seqnum %" PRIu32 "\n", seqnum);
+        LOG_INFO_(", seqnum %" PRIu32 "", seqnum);
+        LOG_INFO_(", RSSI: %d, LQI: %u\n",rssi, lqi);
+
     }
 }
 
 
-static void send_data(uip_ipaddr_t *dest_ipaddr, struct etimer *timer, uint32_t packet_size) {
+static void send_data(uip_ipaddr_t *dest_ipaddr, uint32_t packet_size) {
     static uint32_t seqnum =0;
 
     // uint8_t payload[packet_size] = {0}; // Example payload data
 
     uint8_t payload[packet_size];
-    memset(payload, 0, sizeof(payload));
+    // memset(payload, 0, sizeof(payload));
+    //Insert random data in the packet payload
+    for (int i = 0; i < packet_size; i++) {
+        payload[i] = rand() % 256;
+    }
+    
+    
 
     seqnum++;
     LOG_INFO("Sending to ");
@@ -59,6 +72,16 @@ static void send_data(uip_ipaddr_t *dest_ipaddr, struct etimer *timer, uint32_t 
 
     simple_udp_sendto(&udp_conn, payload, packet_size, dest_ipaddr);
 }
+
+// static struct ctimer restart_timer;
+// static bool is_restarting = false;
+
+// void restart_process(void *ptr) {
+//   process_start(&sensor_node_process, NULL);
+// }
+
+
+
 
 PROCESS(sensor_node_process, "Sensor Node Process");
 AUTOSTART_PROCESSES(&sensor_node_process);
@@ -73,7 +96,11 @@ PROCESS_THREAD(sensor_node_process, ev, data) {
     static uint32_t ps = PACKET_SIZE;
 
     static uip_ipaddr_t dest_ipaddr;
-    static int event_index = -1;
+    static int event_index = 0;
+
+    static radio_value_t incorrect_pan_id = 0xFFFF; // Incorrect PAN ID for leave network
+    static radio_value_t correct_pan_id;
+
 
     PROCESS_BEGIN();
 
@@ -84,62 +111,64 @@ PROCESS_THREAD(sensor_node_process, ev, data) {
     }
     NETSTACK_MAC.on();
 
+
     etimer_set(&send_timer, random_rand() % si);
     etimer_set(&event_timer, event_list[0].time * CLOCK_SECOND);
 
     while(1) {
-        PROCESS_WAIT_EVENT();
-        // PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
-
-        if (ev == PROCESS_EVENT_TIMER) {
-            if(data == &send_timer && NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr) ) {
-                if (send_enable==true){
-                    send_data(&dest_ipaddr, &send_timer, ps);
-                }
-                else{
-                    LOG_INFO("No sending");
-                    LOG_INFO("Send enable: %d\n", send_enable);
-                }
-                etimer_set(&send_timer, si);
-                
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
+        if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)){
+            if(send_enable == true){
+                send_data(&dest_ipaddr, ps);
             }
-            else if(data == &event_timer && event_index < event_list_size){
-                event_index++;
-                LOG_INFO("Event timer expired, event index: %d, event type: %d, current time: %lu\n", event_index, event_list[event_index].event_type, clock_time());
-                // Check the node id 
-                if (node_id == event_list[event_index].node_id || event_list[event_index].node_id == -1) {
-                    // Check if the node should go offline
-                    if (event_list[event_index].event_type == EVENT_FAILURE) {
-                        // leave_rpl_network();
-                        // tsch_disassociate();
-                        NETSTACK_MAC.off(); // Turn off the MAC layer to simulate node going offline
-                        // NETSTACK_RADIO.off();
-                        send_enable = false;
-                        
-                        LOG_INFO("Node %d went offline\n", node_id);
-                    } 
-                    else if (event_list[event_index].event_type == EVENT_RECOVERY) {
-                        NETSTACK_MAC.on(); // Make sure the MAC layer is on
-                        // rejoin_rpl_network();
-                        // tsch_associate();
-                        send_enable = true;
-                        LOG_INFO("Node %d recovered\n", node_id);
-                    }
-                    else if (event_list[event_index].event_type == EVENT_LOAD_VARIATION) {
-                        si = event_list[event_index].target_send_interval * CLOCK_SECOND;
-                        ps = event_list[event_index].target_packet_size;
-                        LOG_INFO("Node %d changed send interval to %" PRIu32 " and packet size to %" PRIu32 "\n", node_id, si, ps);
-                    }
-                }
+            
+            etimer_set(&send_timer, si);
+        }
 
-                // LOG_INFO("event_index: %d\n", event_index);
-                if (event_index < event_list_size){
-                    next_interval = (event_list[event_index].time - event_list[event_index-1].time);
-                    etimer_set(&event_timer, next_interval * CLOCK_SECOND);
+        if(etimer_expired(&event_timer) && event_index < event_list_size){
+            // Check the node id 
+            if (node_id == event_list[event_index].node_id || event_list[event_index].node_id == -1) {
+                LOG_INFO("Event timer expired, event index: %d, event type: %d, current time: %lu\n", event_index, event_list[event_index].event_type, clock_time());
+                // Check if the node should go offline
+                if (event_list[event_index].event_type == EVENT_FAILURE) {
+                    NETSTACK_RADIO.get_value(RADIO_PARAM_PAN_ID, &correct_pan_id);
+                    send_enable = false;
+                    // NETSTACK_ROUTING.leave_network();
+                    // tsch_disassociate();
+                    NETSTACK_RADIO.set_value(RADIO_PARAM_PAN_ID, incorrect_pan_id);
+                    // NETSTACK_MAC.init();
+                    // bool mac_off_success = NETSTACK_MAC.off();
+                    // bool radio_off_success = 
+                    // NETSTACK_RADIO.off();
+                    // tsch_disassociate();
+
+                    // LOG_INFO("MAC off success: %d,  ", mac_off_success);
+                    // LOG_INFO_(", radio_off_succes: %d\n", radio_off_success);
+
+                    
+                    LOG_INFO("Node %d went offline\n", node_id);
+                } 
+                else if (event_list[event_index].event_type == EVENT_RECOVERY) {
+                    NETSTACK_MAC.on(); // Make sure the MAC layer is on
+                    // tsch_associate();
+                    send_enable = true;
+                    NETSTACK_RADIO.set_value(RADIO_PARAM_PAN_ID, correct_pan_id);
+                    LOG_INFO("Node %d recovered\n", node_id);
                 }
+                else if (event_list[event_index].event_type == EVENT_LOAD_VARIATION) {
+                    si = event_list[event_index].target_send_interval * CLOCK_SECOND;
+                    ps = event_list[event_index].target_packet_size;
+                    LOG_INFO("Node %d changed send interval to %" PRIu32 " and packet size to %" PRIu32 "\n", node_id, si, ps);
+                }
+            }
+            event_index++;
+            // LOG_INFO("event_index: %d\n", event_index);
+            if (event_index < event_list_size){
+                next_interval = (event_list[event_index].time - event_list[event_index-1].time);
+                etimer_set(&event_timer, next_interval * CLOCK_SECOND);
             }
         }
-        
+
     }
     PROCESS_END();
 }
